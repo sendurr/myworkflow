703e9ef3856b0a05112aad44c3dc5f010b42d788
sendurr <sendurr@hotmail.com>
2017-04-22 16:06:14 -0700

    added post function

--- models/user_model.js
+++ models/user_model.js
@@ -6,13 +6,13 @@ var userSchema = new Schema({
   first_name: String,
   last_name: String,
   role_id: Number,
-  role: Number
+  role: String,
   department_id: Number,
   department: String,
   updated: { type: Date, default: Date.now },
   created: { type: Date, default: Date.now },
   dob: { type: Date},
-  mobile: Number
+  mobile: String
 });
 
 module.exports.user = mongoose.model("user",userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,25 @@
 var express    = require('express');
 var router = express.Router();
-var User     = require('./app/models/user_model');
+var User = require('../models/user_model').user;
 
 
 router.get('/', function(req, res) {
+    User.find(function (err, users) {
+        if(err)
+            res(500).send(err);
+        res.json({comment:'User created!',data:users});
+    });
     res.json({ message: 'List of users' });
 });
 
 router.post('/', function(req, res) {
-
+    var data = req.body;
+    var user = new User(data);
+    user.save(function(err){
+        if(err)
+            res(500).send(err);
+        res.json({comment:'User created!',data:data});
+    });
 });
 
 
