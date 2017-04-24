362c66a89414b1b243f1b98a5fe97408353296aa
sendurr <sendurr@hotmail.com>
2017-04-24 00:34:26 -0700

    added Oauth function

--- server.js
+++ server.js
@@ -2,12 +2,29 @@
 
 // BASE SETUP
 // =============================================================================
-
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
+
+// OAUTH VERIFICATION
+// =============================================================================
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://sendurr.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'https://sendurr.auth0.com/api/myworkflow/',
+    issuer: "https://sendurr.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 // configure app to use bodyParser()
 // this will let `us get the data from a POST
