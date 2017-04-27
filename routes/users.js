a840b1333c6a6c32dab358d7321de226aad8fced
sendurr <sendurr@hotmail.com>
2017-04-26 22:18:05 -0700

    added pagination

--- routes/users.js
+++ routes/users.js
@@ -39,11 +39,37 @@ var checkPermissions = function(req, res, next){
 
 router.get('/', function(req, res) {
     checkPermissions(req,res);
-    User.find(function (err, users) {
-        if(err)
-            res.status(500).send(err);
-        res.status(200).json({comment:users.length + " users retrieved.",data:users});
-    });
+
+    // parseInt attempts to parse the value to an integer
+    // it returns a special "NaN" value when it is Not a Number.
+    var page = parseInt(req.query.page);
+    if (isNaN(page) || page < 1) {
+        page = 1;
+    }
+    var limit = parseInt(req.query.limit);
+    if (isNaN(limit)) {
+        limit = 10;
+    } else if (limit > 50) {
+        limit = 50;
+    } else if (limit < 1) {
+        limit = 1;
+    }
+    User.find()
+        .limit(limit)
+        .skip(limit * (page-1))
+        .sort({
+            first_name: 'asc',
+            last_name: 'asc',
+            created: 'desc'
+        })
+        .exec(function (err, user) {
+            if(err)
+                res.status(500).send(err);
+            else if(user.length==0)
+                res.status(200).json({comment:"No user found.",data:user});
+            else
+                res.status(200).json({comment:user.length+" users found.",data:user});
+        });
 });
 
 router.post('/', function(req, res) {
@@ -57,17 +83,18 @@ router.post('/', function(req, res) {
     });
 });
 
+
 router.get('/:user_id', function(req, res) {
     checkPermissions(req,res);
-    User.find({'id': req.params.user_id},function (err, user) {
-        if(err)
-            res.status(500).send(err);
-        else if(!user)
-            res.status(200).json({comment:"No user found.",data:user});
-        else
-            res.status(200).json({comment:"user found.",data:user});
-    });
+    User.find({'id': req.params.user_id})
+        .exec(function (err, user) {
+            if(err)
+                res.status(500).send(err);
+            else if(user.length==0)
+                res.status(200).json({comment:"No user found.",data:user});
+            else
+                res.status(200).json({comment:"User found.",data:user});
+        });
 });
-
 
 module.exports = router;
